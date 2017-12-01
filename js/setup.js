f2d833eb5e331901a52e2cb2fec491ce4e649c07
Danila Pichnovsky <essencer4@mail.ru>
2017-12-01 05:55:36 +0300

    Урок 4|module4-task1, учебный проект|попытка №1

--- js/setup.js
+++ js/setup.js
@@ -43,6 +43,14 @@ var WIZARDS_EYES_COLORS = [ // цвета глаз
   'green'
 ];
 
+var WIZARDS_FIREBALL_COLORS = [ // цвета файрболлов
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
+];
+
 // Функции.
 // Функция генерации случайных данных (слямзена у Кантора)
 
@@ -123,6 +131,128 @@ container.appendChild(fragment);
 
 // Убираем у нужных html-блоков css-класс 'hidden' чтобы отобразить данные html-блоки
 
-document.querySelector('.setup').classList.remove('hidden');
+// document.querySelector('.setup').classList.remove('hidden');
 
-document.querySelector('.setup-similar').classList.remove('hidden');
+// document.querySelector('.setup-similar').classList.remove('hidden');
+
+// ////////////////////////////////////////////////////////////////////////////////////
+
+// Задание 4. Сценарии взаимодействия пользователя с сайтом.
+
+// Нажатие на элемент .setup-open приводит
+// к появлению диалогового окна .setup.
+// Окно спрятано с помощью CSS-класса hidden,
+// поэтому, чтобы показать, нужно удалить
+// соответствующий класс
+
+// Нажатие на элемент .setup-open удаляет класс hidden
+// у блока setup
+
+// Создадим переменные, которые хранят элементы, которыми мы будем оперировать
+
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
+var setupOpen = document.querySelector('.setup-open');
+var setup = document.querySelector('.setup');
+var setupClose = setup.querySelector('.setup-close');
+var setupUserName = document.querySelector('.setup-user-name');
+
+// Теперь, опишем реакцию блока .setup-open и .setup-close на нажатие.
+// Зададим функции для закрытия и открытия попапа.
+
+var onPopupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+    if (setupUserName === document.activeElement) {
+      evt.stopPropagation();
+    }
+  }
+};
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) { // открытие попапа по нажатию enter
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function (evt) { // закрытие попапа по нажатию enter
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+
+// Переводим ошибки на русский язык
+
+var userNameInput = setup.querySelector('.setup-user-name');
+
+userNameInput.addEventListener('invalid', function (evt) {
+  if (userNameInput.validity.tooShort) {
+    userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else if (userNameInput.validity.tooLong) {
+    userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+  } else if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+userNameInput.addEventListener('input', function (evt) {
+  var target = evt.target;
+  if (target.value.length < 2) {
+    target.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else {
+    target.setCustomValidity('');
+  }
+});
+
+// 3. Изменение цвета мантии персонажа по нажатию
+
+var colorRobe = document.querySelector('.wizard-coat');
+
+colorRobe.addEventListener('click', function () {
+  colorRobe.style.fill = pickRandomNotUniqueItem(WIZARDS_COAT_COLORS);
+});
+
+// Функция для выбора случайного НЕ уникального элемента массива
+
+var pickRandomNotUniqueItem = function (array) {
+  var currentIndex = getRandomNumber(0, array.length); // обращаемся к рандомайзеру
+  return array[currentIndex];
+};
+
+
+// 4. Изменение цвета глаз персонажа по нажатию.
+
+var colorEye = document.querySelector('.wizard-eyes');
+
+colorEye.addEventListener('click', function () {
+  colorEye.style.fill = pickRandomNotUniqueItem(WIZARDS_EYES_COLORS);
+});
+
+// 5. Изменение цвета файрболла персонажа по нажатию.
+
+var colorFireball = document.querySelector('.setup-fireball-wrap');
+
+colorFireball.addEventListener('click', function () {
+  colorFireball.style.background = pickRandomNotUniqueItem(WIZARDS_FIREBALL_COLORS);
+});
